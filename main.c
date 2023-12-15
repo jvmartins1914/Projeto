51e9a57abb97fc4d05d5a6e9f560e81be23e565c
jvmartins1914 <138832040+jvmartins1914@users.noreply.github.com>
2023-12-15 17:36:04 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,28 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <dirent.h>
-
-#define DIRETORIO_TABELAS "tabelas"
-
-typedef struct {
-    char nome[50];
-    char tipo[10];
-} Coluna;
-
-typedef struct {
-    unsigned int chavePrimaria;
-    char **dados;  
-} Registro;
-
-typedef struct {
-    char nomeTabela[50];
-    Coluna *colunas;
-    unsigned int numColunas;
-    Registro *registros;
-    unsigned int numRegistros;
-} Tabela;
+#include "tabelas.h"
 
 void criarDiretorio() {
   
